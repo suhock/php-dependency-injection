067a0a8142e87137ece6ac8dbb8dacde1f15d868
Matthew Suhocki <matt@fivetwo.org>
2022-05-27 13:36:17 +0900

    Added return types to tests

--- tests/FiveTwo/DependencyInjection/DependencyInjectionExceptionTest.php
+++ tests/FiveTwo/DependencyInjection/DependencyInjectionExceptionTest.php
@@ -12,20 +12,20 @@ use PHPUnit\Framework\TestCase;
 
 class DependencyInjectionExceptionTest extends TestCase
 {
-    public function test__construct_NoComposition()
+    public function test__construct_NoComposition(): void
     {
         $exception = new DependencyInjectionException("Message1");
         self::assertSame("Message1", $exception->getMessage());
     }
 
-    public function test__construct_Composition()
+    public function test__construct_Composition(): void
     {
         $exception = new DependencyInjectionException("Message2", new DependencyInjectionException("Message1"));
         self::assertMatchesRegularExpression("/Message2.*Message1/s", $exception->getMessage());
         self::assertNull($exception->getPrevious());
     }
 
-    public function test__construct_NoCompositionForUnrelated()
+    public function test__construct_NoCompositionForUnrelated(): void
     {
         $exception = new DependencyInjectionException("Message2", $previous = new Exception("Message1"));
         self::assertSame("Message2", $exception->getMessage());
