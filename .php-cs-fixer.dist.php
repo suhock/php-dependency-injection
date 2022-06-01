2a56e95c45c827f36fef4f79ce16c54ae4dab111
Matthew Suhocki <matt@fivetwo.org>
2022-06-01 23:34:57 +0900

    Updates for static analysis tool configs

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * Copyright (c) 2022 Matthew Suhocki. All rights reserved.
+ * Copyright (c) 2022 Five Two Foundation, LLC. All rights reserved.
+ *
+ * This software is licensed under the terms of the MIT License <https://opensource.org/licenses/MIT>.
+ * The above copyright notice and this notice shall be included in all copies or substantial portions of this software.
+ */
+
+declare(strict_types=1);
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'strict_param' => true,
+        'array_syntax' => ['syntax' => 'short'],
+    ])
+    ->setFinder(
+        PhpCsFixer\Finder::create()
+            ->exclude('vendor')
+            ->in(__DIR__)
+    );
+
